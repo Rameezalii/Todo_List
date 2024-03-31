29e091bb4f9b00f17e25947648eb86568ad3e8c0
Rameez Ali <ra2252252@gmail.com>
2024-03-31 07:49:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let Todos = [];
+// let condition = true;
+console.log(`\t"Welcome to Todo List"`);
+while (true) {
+    let addTasks = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todos?",
+        },
+        {
+            name: "options",
+            type: "list",
+            message: "Do you want to more?",
+            choices: ["Add more", "Delete", "Exit"]
+        },
+    ]);
+    if (addTasks.options === "Add more") {
+        Todos.push(addTasks.todo);
+        console.log("Task added successfully!");
+        console.log(Todos);
+    }
+    else if (addTasks.options === "Delete") {
+        if (Todos.length === 0) {
+            console.log("No tasks to delete!");
+        }
+        else {
+            Todos.pop();
+            console.log("Last task deleted successfully!");
+            console.log(Todos);
+        }
+    }
+    else if (addTasks.options === "Exit") {
+        console.log(`Exiting...`);
+        break;
+    }
+}
